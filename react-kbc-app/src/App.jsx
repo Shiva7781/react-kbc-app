3c83674cef466de42c3161d1515771b67969209a
Shiva7781 <shivavishwakarma7781@gmail.com>
2022-11-24 20:49:20 +0530

    Sounds Added

--- react-kbc-app/src/App.jsx
+++ react-kbc-app/src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
+import Start from "./components/Start";
+
+import tiviaData from "./tiviaData";
 
 function App() {
-  const [questionNumber, setQuestionNumber] = useState(7);
+  const [username, setUsername] = useState(null);
+  const [stop, setStop] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [earned, setEarned] = useState("₹ 0");
+
+  // console.log(tiviaData);
 
   const moneyPyramid = [
     { no: 1, amount: "₹ 1000" },
@@ -23,32 +32,57 @@ function App() {
     { no: 15, amount: "₹ 100000000" },
   ].reverse();
 
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.no === questionNumber - 1).amount);
+  }, [moneyPyramid, questionNumber]);
+
   return (
-    <div className="App">
-      <div className="Main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom">
-          <Trivia />
-        </div>
-      </div>
-      <div className="Money_pyramid">
-        <ul className="moneyList">
-          {moneyPyramid.map((money) => (
-            <li
-              className={
-                questionNumber === money.no
-                  ? "moneyListItem active"
-                  : "moneyListItem"
-              }
-            >
-              <span className="moneyListItemNumber">{money.no}</span>
-              <span className="moneyListItemAmount">{money.amount}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+    <div className={!username ? "Start_Game" : "App"}>
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="Main">
+            {stop ? (
+              <h1 className="endText"> You earned {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    setStop={setStop}
+                    tiviaData={tiviaData}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="Money_pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((money, i) => (
+                <li
+                  key={i}
+                  className={
+                    questionNumber === money.no
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{money.no}</span>
+                  <span className="moneyListItemAmount">{money.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- react-kbc-app/src/components/Trivia.jsx
+++ react-kbc-app/src/components/Trivia.jsx
@@ -1,25 +1,94 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../assets/sounds/start_play.mp3";
+import correct from "../assets/sounds/correct_answer.mp3";
+import wrong from "../assets/sounds/wrong_answer.mp3";
 
-const Trivia = () => {
+const Trivia = ({ tiviaData, setStop, questionNumber, setQuestionNumber }) => {
+  const [question, setQuestion] = useState(null);
+  const [selectAnswer, setSelectAnswer] = useState(null);
+  const [mclassName, setMclassName] = useState("answers");
+
+  const [letplay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
+
+  useEffect(() => {
+    letplay();
+  }, [letplay]);
+
+  useEffect(() => {
+    setQuestion(tiviaData[questionNumber - 1]);
+  }, [tiviaData, questionNumber]);
+
+  const delay = (duration, callBack) => {
+    setTimeout(() => {
+      callBack();
+    }, duration);
+  };
+
+  const handleCheck = (x) => {
+    // console.log(x);
+
+    setSelectAnswer(x);
+    setMclassName("answers active");
+
+    delay(3000, () =>
+      setMclassName(
+        question?.answers[x].correct ? "answers correct" : "answers wrong"
+      )
+    );
+
+    delay(5000, () => {
+      if (question.answers[x].correct) {
+        correctAnswer();
+
+        delay(1000, () => {
+          setQuestionNumber((prev) => prev + 1);
+          setSelectAnswer(null);
+        });
+      } else {
+        wrongAnswer();
+
+        delay(1000, () => {
+          setStop(true);
+        });
+      }
+    });
+  };
+
   return (
     <div className="trivia">
-      <div className="question">Question </div>
+      <div className="question">{question?.question} </div>
       <div className="options">
-        <div className="answers wrong">
+        <div
+          className={selectAnswer === 0 ? mclassName : "answers"}
+          onClick={() => handleCheck(0)}
+        >
           <span>a) </span>
-          <span>A</span>
+          <span>{question?.answers[0].text}</span>
         </div>
-        <div className="answers correct">
+        <div
+          className={selectAnswer === 1 ? mclassName : "answers"}
+          onClick={() => handleCheck(1)}
+        >
           <span>b) </span>
-          <span>B</span>
+          <span>{question?.answers[1].text}</span>
         </div>
-        <div className="answers">
+        <div
+          className={selectAnswer === 2 ? mclassName : "answers"}
+          onClick={() => handleCheck(2)}
+        >
           <span>c) </span>
-          <span>C</span>
+          <span>{question?.answers[2].text}</span>
         </div>
-        <div className="answers">
+        <div
+          className={selectAnswer === 3 ? mclassName : "answers"}
+          onClick={() => handleCheck(3)}
+        >
           <span>d) </span>
-          <span>D</span>
+          <span>{question?.answers[3].text}</span>
         </div>
       </div>
     </div>
